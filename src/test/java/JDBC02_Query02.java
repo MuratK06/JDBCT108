5398ca55e22b6710a49b59f6137aa3121a26a39e
Murat <muratkorkmaz8306@gmail.com>
2023-01-30 20:30:17 +0100

    2

--- src/test/java/JDBC02_Query02.java
+++ src/test/java/JDBC02_Query02.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-public class JDBC01_Query01_0 {
+public class JDBC02_Query02 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         //1-Ilgili driver'i yüklemeliyiz.Msql kullandigimizi bildiriyoruz.
         // Driver'i bulamama ihtimaline karsi  forName metodu icin ClassNotFoundException
@@ -13,13 +13,13 @@ public class JDBC01_Query01_0 {
         //SQLException firlatmamizi istiyor.
 
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys?serverTimezone=UTC",
-                                                    "root", "1234");
+                "root", "1234");
         //Select * from ogrenciler;
         //3-SQL query'leri icin bir Statement objesi olusturup,javada sql sorgularimiz icin bir alan acacagiz.
 
         Statement st = con.createStatement();
         // 4-SQL query'lerimizi yazip calistirabiliriz.
-        ResultSet veri = st.executeQuery("select * from personel");
+        ResultSet veri = st.executeQuery("select * from isciler");
         // 5-Sonuclari görmek icin Iteration ile Set icerisindeki elemanlari while döngusu ile yazdiriyoruz.
 
         while (veri.next()) {
@@ -29,13 +29,9 @@ public class JDBC01_Query01_0 {
         }
         //6-Olusturdugumuz nesneleri kapatalim
 
-        /*
         st.close();
         con.close();
         veri.close();
-
-         */
-
-
     }
+
 }
